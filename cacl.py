6f675776f1dedec62de5fa732d46332f7c18d3c2
SajjadSeifi <s.09334497255@gmail.com>
2022-02-14 22:54:06 -0800

    build lexer level

--- cacl.py
+++ cacl.py
@@ -31,8 +31,101 @@
     
     comment := #[^\n]*
 '''
+#Lexical
+import re
 
-code = input()
-while code.upper() != 'E':
-    print("code : %s" % code)
-    code = input()
+#regex
+idpat = re.compile('/^[A-Za-z_][A-Za-z_0-9]*$/')
+numpat = re.compile('/^[0-9]+$/')
+
+class Lexer:
+    def __init__(self, file):
+        self.file = open(file,'r')
+    
+    def seekprev(self):
+        tel = self.file.tell()
+        self.file.seek(tel-1,0)
+
+    def seeknext(self):
+        self.nextch()
+    
+    def nextch(self):
+        return self.file.read(1)
+
+    def gettoken(self):
+        saved = self.file.tell()
+        token = self.droptoken()
+        self.file.seek(saved,0)
+        return token
+
+    def droptoken(self):
+        chunk = self.nextch()
+        self.seekprev()
+
+        if(chunk.isspace()):
+            self.whitespace()
+            return self.droptoken()
+        elif(chunk.isalpha()):
+            return self.iden()
+        elif(chunk.isnumeric()):
+            return self.num()
+        elif(chunk == "#"):
+            self.comment()
+            return self.droptoken()
+        elif(chunk == ''):
+            self.seeknext()
+            return chunk
+        else:
+            self.seeknext()
+            return chunk
+
+    def whitespace(self):
+        c = self.nextch()
+        while(c .isspace()):
+            c = self.nextch()
+
+        self.seekprev()
+    def comment(self):
+        c = ''
+
+        while(c != '\n'):
+            c = self.nextch()
+
+    def iden(self):
+        c = self.nextch()   
+        if not c.isalpha():
+            self.seekprev()    
+            return 
+
+        tok = ''
+        while(c.isalpha() or c.isnumeric()):
+            tok += c
+            c = self.nextch()
+        
+        if(c != ''):
+            self.seekprev()
+        return tok
+
+    def num(self):
+        tok = ''
+        c = self.nextch()
+
+        while(c.isnumeric()):
+            tok += c
+            c = self.nextch()
+
+                
+        if(c != ''):
+            self.seekprev()
+
+        return tok
+
+
+#compiler
+lex = Lexer("./test/0")
+
+while True:
+    t = lex.droptoken()
+    if not t:
+        break
+    print("token : %s" % t)
