411036e0877d28abe15b17d099fc4540ab083731
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 08:34:30 -0800

    build calc funtionality

--- cacl/interpreter.py
+++ cacl/interpreter.py
@@ -3,6 +3,11 @@ import ast_calc as AST
 
 def interpret(ast : AST.Node):
     if isinstance(ast,AST.ASTCalc):
+        calc(ast)
         print("finished");
     else:
         print("ast error: please entered root ast calc");
+
+def calc(ast : AST.Node):
+    if len(ast.children):
+        interpret(ast.children[1])
