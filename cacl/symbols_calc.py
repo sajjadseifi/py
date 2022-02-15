3e3d6f72677e85bbb2ad4d3f522d0dcba0087df6
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 09:04:48 -0800

    fixed symbols::put

--- cacl/symbols_calc.py
+++ cacl/symbols_calc.py
@@ -5,11 +5,7 @@ class Symbols:
     def get(self,name):
         return self.tbl.get(name) 
     def put(self,name,value):
-        if(self.exist(name)):
-            return False 
-
         self.tbl[name] = value
-        return True 
 
     def rm(self,name):
         self.tbl.pop(name)
