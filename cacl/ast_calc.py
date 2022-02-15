34d66fd936a2a272f41792f538e723253e582779
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 09:31:32 -0800

    mov args to local prop

--- cacl/ast_calc.py
+++ cacl/ast_calc.py
@@ -41,46 +41,61 @@ class ASTCalc0( ASTCalc ):
         ASTCalc.__init__(self)
 
 class ASTCalc1( ASTCalc ):
-    def __init__( self,stmt : ASTStmt,cacl : ASTCalc ):
+    def __init__( self,stmt : ASTStmt,calc : ASTCalc ):
         ASTCalc.__init__(self)
-        self.children = [stmt,cacl]
+        self.stmt = stmt
+        self.calc = calc
+        self.children = [stmt,calc]
 
 class ASTStmtExpr( ASTStmt ):
     def __init__( self,expr : ASTExpr ):
         ASTStmt.__init__(self)
+        self.expr = expr
         self.children = [expr]
 
 class ASTStmtPrint( ASTStmt ):
     def __init__( self,expr : ASTExpr ):
         ASTStmt.__init__(self)
+        self.expr = expr
         self.children = [expr]
 
 class ASTIden( ):
     def __init__( self,name ):
         ASTExpr.__init__(self)
+        self.name = name
         self.children = [name]
 
 class ASTNum( ASTExpr ):
     def __init__( self,num ):
         ASTExpr.__init__(self)
+        self.num = num
         self.children = [num]
 
 class ASTExprAss( ASTExpr ):
     def __init__( self,iden : ASTIden,expr : ASTExpr ):
         ASTExpr.__init__(self)
+        self.iden = iden
+        self.expr = expr
         self.children = [iden,expr]
 
 class ASTExprUnary( ASTExpr ):
     def __init__( self,oprator : str,expr : ASTExpr ):
         ASTExpr.__init__(self)
+        self.oprator = oprator
+        self.expr = expr
         self.children = [oprator,expr]
 
 class ASTExprCacluate( ASTExpr ):
     def __init__( self,left : ASTExpr,oprator : str,right : ASTExpr ):
         ASTExpr.__init__(self)
+        self.left = left
+        self.oprator = oprator
+        self.right = right
         self.children = [left,oprator,right]
 
 class ASTExprCall( ASTExpr ):
     def __init__( self,iden : ASTIden,expr : ASTExpr ):
         ASTExpr.__init__(self)
+        self.iden = iden
+        self.expr = expr
         self.children = [iden,expr]
