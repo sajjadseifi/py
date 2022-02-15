77bdbd03d046b3697a20c8cf2938a0ab79875fc1
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 10:42:19 -0800

    fixed get value in dirct

--- cacl/value_calc.py
+++ cacl/value_calc.py
@@ -1,6 +1,6 @@
 import ast_calc as AST
 
 def valget(ast : AST.Node):
-    return ast.value["val"]    
+    return ast.value.get("val")    
 def valset(ast : AST.Node,val):
     ast.value["val"] = val
