8b87349468a8f47feec0bb50c03d055570d2697b
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 10:43:37 -0800

    add pre parser & interpreter

--- cacl/cacl.py
+++ cacl/cacl.py
@@ -31,10 +31,13 @@
 from os.path import abspath
 from lexer_calc import Lexer
 from parser_tree_calc import ParserTree
+from pre_parser_calc import pre_parse
+from interpreter import interpret
 
 faddr = "./test/0"
 addr = abspath(faddr)
 lex = Lexer(addr)
 pars = ParserTree(lex)
 ast = pars.parse()
-print(ast)
+pre_parse(ast)
+interpret(ast)
