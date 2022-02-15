9dff1e5e3d2a0c23672e74cd02827e6012189703
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 08:34:49 -0800

    build stmt functionality

--- cacl/interpreter.py
+++ cacl/interpreter.py
@@ -10,4 +10,13 @@ def interpret(ast : AST.Node):
 
 def calc(ast : AST.Node):
     if len(ast.children):
+        stmt(ast.children[0])
         interpret(ast.children[1])
+    
+def stmt(ast : AST.Node):
+    if isinstance(ast,AST.ASTStmtExpr):
+        pass
+    elif isinstance(ast,AST.ASTStmtPrint):
+        pass
+    else:
+        print("ast error: this ast is not stmt");
