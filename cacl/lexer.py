677a04649540f147876e04ec2c4111b4582f4c1f
SajjadSeifi <s.09334497255@gmail.com>
2022-02-15 07:20:55 -0800

    add token lexical grammar

--- cacl/lexer.py
+++ cacl/lexer.py
@@ -1,10 +1,17 @@
 #Lexical Token
 '''
+    token := + |
+             - |
+             * |
+             / |
+             %
+
     iden := [A-Za-z_][A-Za-z_0-9]*
 
     num  := [0-9]+
     
     comment := #[^\n]*
+
 '''
 
 from cacl.token import Token
