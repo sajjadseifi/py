a4f63176561c4bbcc7a375a4e7fe2212fc9f2ded
SajjadSeifi <s.09334497255@gmail.com>
2022-02-16 12:50:41 -0800

    init lexer test module(ok)

--- lifepath/main.py
+++ lifepath/main.py
@@ -25,3 +25,11 @@
         PER     |
         PRINT
 '''
+
+from posixpath import abspath
+from lexer_lp import Lexer
+from test_lp import test_lexer
+
+addr = abspath("./test/0")
+lex = Lexer(addr)
+test_lexer(lex)
