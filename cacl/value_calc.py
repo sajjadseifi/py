55529aa184b93a488ff544f20e0ce76615e31a87
SajjadSeifi <s.09334497255@gmail.com>
2022-02-16 23:32:08 -0800

    impliment index getter setter semantic value

--- cacl/value_calc.py
+++ cacl/value_calc.py
@@ -2,5 +2,12 @@ import ast_calc as AST
 
 def valget(ast : AST.Node):
     return ast.value.get("val")    
+
 def valset(ast : AST.Node,val):
     ast.value["val"] = val
+
+def idxget(ast : AST.Node):
+    return ast.value.get("idx")    
+
+def idxset(ast : AST.Node,idx):
+    ast.value["idx"] = idx
