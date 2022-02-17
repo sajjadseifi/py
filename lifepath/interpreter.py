5f0cbc476e42dc3b204e5f7b4cb080f502d192f4
SajjadSeifi <s.09334497255@gmail.com>
2022-02-16 21:31:29 -0800

    build:stmt body

--- lifepath/interpreter.py
+++ lifepath/interpreter.py
@@ -11,5 +11,23 @@ def lp(ast : AST.Node):
         stmt(ast.first())
         interpreter(ast.children[1])
 
-def stmt():
-    pass
+def stmt(ast : AST.Node):
+    num = None
+    if len(ast.children) > 0:
+        num = ast.children[0]
+
+    if isinstance(ast,AST.ASTStmtPush):
+        pass
+    if isinstance(ast,AST.ASTStmtPlus):
+        pass
+    if isinstance(ast,AST.ASTStmtMin):
+        pass
+    if isinstance(ast,AST.ASTStmtMul):
+        pass
+    if isinstance(ast,AST.ASTStmtDiv):
+        pass
+    if isinstance(ast,AST.ASTStmtPer):
+        pass
+    if isinstance(ast,AST.ASTStmtPrint):
+        pass
+
