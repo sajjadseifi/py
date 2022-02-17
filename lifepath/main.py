4e6f43f4e0fa5a5c2d3d2df605998b292a236ed7
SajjadSeifi <s.09334497255@gmail.com>
2022-02-16 23:02:39 -0800

    add command line arg for get input on file

--- lifepath/main.py
+++ lifepath/main.py
@@ -27,10 +27,12 @@ from posixpath import abspath
 from lexer_lp import Lexer
 from interpreter_lp import interpreter
 from parse_tree_lp import ParseTree
+import sys
 
-addr = abspath("./test/0")
-lex = Lexer(addr)
-p3 = ParseTree(lex)
-ast = p3.parse()
-print(ast)
-interpreter(ast)
+if len(sys.argv):
+    path = sys.argv[1]
+    addr = abspath(path)
+    lex = Lexer(addr)
+    p3 = ParseTree(lex)
+    ast = p3.parse()
+    interpreter(ast)
