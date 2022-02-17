8b8eec153fed2c5f10622d8a401a519e2ea5b385
SajjadSeifi <s.09334497255@gmail.com>
2022-02-17 01:46:52 -0800

    impliemnt ir writer function

--- cacl/ir_calc.py
+++ cacl/ir_calc.py
@@ -2,20 +2,30 @@ class IR:
     def __init__(self,path) -> None:
         self.idxstk = 0
         self.out = open(path,"w+")
+
+    def w(self,s:str):
+        self.out.write(s)
     
-    def push(num):
-        pass
-    def mov(tar,src):
-        pass
-    def print(num):
-        pass
-    def plus():
-        pass
-    def min():
-        pass
-    def div():
-        pass
-    def mul():
-        pass
-    def per():
-        pass
+    def push(self,num):
+        self.w("PUSH %s" % num)
+
+    def mov(self,tar,src):
+        self.w("MOV %s %s" % (tar,src))
+    
+    def print(self):
+        self.w("PRINT")
+    
+    def plus(self):
+        self.w("PLUS")
+    
+    def min(self):
+        self.w("MIN")
+    
+    def div(self):
+        self.w("DIV")
+    
+    def mul(self):
+        self.w("MUL")
+    
+    def per(self):
+        self.w("PER")
