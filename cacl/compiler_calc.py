2279d95adba87a84a52a4f20d3a75e6414fc3193
SajjadSeifi <s.09334497255@gmail.com>
2022-02-17 02:07:03 -0800

    add ir module to compiler

--- cacl/compiler_calc.py
+++ cacl/compiler_calc.py
@@ -1,101 +1,94 @@
 import ast_calc as AST
+from cacl.ir_calc import IR
 from value_calc import valget, valset
 from symbols_calc import Symbols 
 #semantic
 symtbl = Symbols()
-oprcode = {
-    "+" :"PLUS",
-    "-" :"MIN",
-    "*" :"MUL",
-    "/" :"DIV",
-    "%" :"PER",
-}
 
-class STK:
-    def __init__(self,c:int) -> None:
-        self.c = c
-    def up(self):
-        self.c +=1
-    def down(self):
-        self.c +=1
 
-stk = STK(-1)
-
-def pushstk(num):
-    print("PUSH",num)
-    stk.up()
-
-def popstk():
-    stk.down()
-
-def compiler(ast : AST.Node):
+def compiler(ir:IR,ast : AST.Node):
     if isinstance(ast,AST.ASTCalc):
-        calc(ast)
+        calc(ir,ast)
     else:
         print("ast error: please entered root ast calc");
 
-def calc(ast : AST.Node):
+def calc(ir:IR,ast : AST.Node):
     if len(ast.children):
-        stmt(ast.first())
+        stmt(ir,ast.first())
         compiler(ast.children[1])
     
-def stmt(ast : AST.ASTStmt):
+def stmt(ir:IR,ast : AST.ASTStmt):
     if isinstance(ast,AST.ASTStmtExpr):
-        expr(ast.first())
+        expr(ir,ast.first())
     elif isinstance(ast,AST.ASTStmtPrint):
         cast = ast.first()
-        expr(cast)
-        print("PRINT")
+        expr(ir,cast)
+        ir.print()
     else:
         print("ast error: this ast is not stmt");
 
-def expr(ast : AST.Node):
+def expr(ir:IR,ast : AST.Node):
     if isinstance(ast,AST.ASTExprAss):
-        exprass(ast)    
+        exprass(ir,ast)    
     elif isinstance(ast,AST.ASTExprUnary):
-        exprunary(ast)    
+        exprunary(ir,ast)    
     elif isinstance(ast,AST.ASTExprCacluate):
-        exprcalc(ast)    
+        exprcalc(ir,ast)    
     elif isinstance(ast,AST.ASTExprCall):
-        exprmath(ast)    
+        exprmath(ir,ast)    
     elif isinstance(ast,AST.ASTNum) or isinstance(ast,AST.ASTIden):
-        exprprim(ast)    
+        exprprim(ir,ast)    
 
-def exprass(ast : AST.ASTExprAss):
+def exprass(ir:IR,ast : AST.ASTExprAss):
     name = ast.iden.name
     expr(ast.expr)
     
     idx = symtbl.get(name)
-    if idx:
-        print("MOV",idx,stk.c)
-    else:
-        symtbl.put(name,stk.c)
 
-def exprunary(ast : AST.ASTExprUnary):
+    if idx:
+        ir.mov(idx,ir.idxstk)
+    else:
+        symtbl.put(name,ir.idxstk)
+
+def exprunary(ir:IR,ast : AST.ASTExprUnary):
     opr = ast.oprator
     expr(ast.expr)
 
-    pushstk(opr + 1)
-    print("MUL")
-    popstk()
+    ir.push(opr + 1)
+    ir.mul()
+    ir.down()
 
-def exprcalc(ast : AST.ASTExprCacluate):
+def exprcalc(ir:IR,ast : AST.ASTExprCacluate):
     #sub expersion
     expr(ast.left)
     expr(ast.right)
-    #print
-    print(oprcode.get(ast.oprator))
-    popstk()
 
-def exprmath(ast : AST.ASTExprCall):
+    #print
+    op = ast.oprator 
+
+    if op == "+":
+        ir.plus()
+    if op == "-":
+        ir.min()
+    if op == "*":
+        ir.mul()
+    if op == "/":
+        ir.div()
+    if op == "%":
+        ir.per()
+
+    ir.down()
+
+def exprmath(ir:IR,ast : AST.ASTExprCall):
     pass
 
-def exprprim(ast : AST.Node):
+def exprprim(ir:IR,ast : AST.Node):
     if isinstance(ast,AST.ASTNum):
-        pushstk(ast.num)
+        ir.push(ast.num)
+
     elif isinstance(ast,AST.ASTIden):
         name = ast.name
         idx  = symtbl.get(name)
         if isinstance(idx,int) and idx > -1:
-            stk.up()
-            print("MOV",stk.c,idx)
+            ir.up()
+            ir.mov(ir.idxstk,idx)
