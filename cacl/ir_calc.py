24eb6ea664f3e7d33c1f704c0bb35b6addaeb3d2
SajjadSeifi <s.09334497255@gmail.com>
2022-02-17 01:41:13 -0800

    init ir code calculator module

---
+++ cacl/ir_calc.py
@@ -0,0 +1,21 @@
+class IR:
+    def __init__(self,path) -> None:
+        self.idxstk = 0
+        self.out = open(path,"w+")
+    
+    def push(num):
+        pass
+    def mov(tar,src):
+        pass
+    def print(num):
+        pass
+    def plus():
+        pass
+    def min():
+        pass
+    def div():
+        pass
+    def mul():
+        pass
+    def per():
+        pass
